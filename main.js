c4dae088406c533764a5c777f215911e64b53707
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-12-18 22:18:51 +0900

    delete img

--- main.js
+++ main.js
@@ -8,12 +8,10 @@ var app = new Vue({
             { url: "#works", text: "Works"},
             { url: "#interests", text: "Interests"},
         ],
-        hard: [
+        languages: [
             { lang: "Creo", src: "./images/Creo.png", comment: "機械設計でよく使ってます．"},
             { lang: "KiCAD", src: "./images/Kicad.png", comment: "回路設計と基板設計に使ってます．"},
             { lang: "Blender", src: "./images/Blender.png", comment: "最近使ってます．勉強中です．"},
-        ],
-        languages: [
             { lang: "C/C++", src: "./images/C++.png", comment: "組み込みと競技プログラミングで使ってます．"},
             { lang: "Python", src: "./images/Python.png", comment: "自動化で使ってます．機械学習は勉強中です．"},
             { lang: "Ruby on Rails", src: "./images/Rails.png", comment: "サーバーサイドフレームワークとして勉強中です．"},
