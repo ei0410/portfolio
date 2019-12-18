f5391373177efd20e969416699ba059b8c5cf43b
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-12-18 15:15:15 +0900

    add language list

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-        contentslist: ["about", "skills", "event", "interests", "links"],
+        contentslist: [
+            { url: "#about", text: "about"},
+            { url: "#skills", text: "skills"},
+            { url: "#event", text: "event"},
+            { url: "#interests", text: "interests"},
+            { url: "#links", text: "links"},
+        ],
+        language: [
+            { lang: "C/C++", comment: "C"},
+            { lang: "Python", comment: "Py"},
+            { lang: "Ruby on Rails", comment: "R"},
+            { lang: "Vue.js", comment: "V"},
+            { lang: "Verilog", comment: "Ve"},
+            { lang: "Processing", comment: "Pr"},
+        ],
         linklist: [
             { url: "https://twitter.com/ei01241", text: "twitter" },
             { url: "https://github.com/ei0410", text: "github" },
