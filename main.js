b340f1957140a3150fd778878bc75e223a188cb2
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-12-18 13:23:51 +0900

    add css and js

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        contentslist: ["about", "skills", "event", "interests", "links"],
+        linklist: ["https://twitter.com/ei01241", "http://ei0124.blog.fc2.com/", "https://github.com/ei0410"]
+    }
+})
