6ecab3ed6261c264659afdc5b5048d063e385128
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-12-18 17:15:29 +0900

    add images and skill-language

--- main.js
+++ main.js
@@ -2,24 +2,49 @@ var app = new Vue({
     el: '#app',
     data: {
         contentslist: [
-            { url: "#about", text: "about"},
-            { url: "#skills", text: "skills"},
-            { url: "#event", text: "event"},
-            { url: "#interests", text: "interests"},
-            { url: "#links", text: "links"},
+            { url: "#about", text: "About"},
+            { url: "#skills", text: "Skills"},
+            { url: "#works", text: "Works"},
+            { url: "#events", text: "Events"},
+            { url: "#interests", text: "Interests"},
         ],
-        language: [
-            { lang: "C/C++", comment: "C"},
-            { lang: "Python", comment: "Py"},
-            { lang: "Ruby on Rails", comment: "R"},
-            { lang: "Vue.js", comment: "V"},
-            { lang: "Verilog", comment: "Ve"},
-            { lang: "Processing", comment: "Pr"},
+        languages: [
+            { lang: "C/C++", src: "./images/C++.png", comment: "組み込みと競技プログラミングで使っています．一番好きです．"},
+            { lang: "Python", src: "./images/Python.png", comment: "自動化で使っています．機械学習は勉強中です．"},
+            { lang: "Ruby on Rails", src: "./images/Rails.png", comment: "webのサーバーサイドフレームワークとして勉強中です．"},
+            { lang: "Vue.js", src: "./images/Vue.png", comment: "webのフロントエンドフレームワークとして勉強中です．"},
+            { lang: "ROS", src: "./images/ROS.png", comment: "ロボット制御で使っています．"},
+            { lang: "Arduino", src: "./images/Arduino.png", comment: "組み込みで簡単な制御をしたいときに使っています．"},
+            { lang: "Processing", src: "./images/Processing.png", comment: "アート作品で使っています．"},
+            { lang: "Verilog", src: "./images/Verilog.png", comment: "CPUを作るために勉強中です．"},
         ],
         linklist: [
-            { url: "https://twitter.com/ei01241", text: "twitter" },
-            { url: "https://github.com/ei0410", text: "github" },
-            { url: "http://ei0124.blog.fc2.com", text: "blog" },
+            { url: "https://twitter.com/ei01241", src: "./images/Twitter.png" },
+            { url: "https://instagram.com/ei01241", src: "./images/Instagram.png" },
+            { url: "https://github.com/ei0410", src: "./images/Github.png" },
+            { url: "http://ei0124.blog.fc2.com", src: "./images/FC2.png" },
         ]
     }
 })
+
+
+/*
+var component = {
+　data: function(){
+　　　return {
+　　　　　count: 0
+　　　}
+  },
+  template: "<p>{{count}}<button @click='increment'>+1</button></p>",
+  methods: {
+    increment: function(){
+        this.count += 1;
+    }
+  }
+}
+    
+Vue.component("sample-component",component)
+new Vue({
+    el: "#app",
+})
+*/
