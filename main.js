c19b75473bdc38c6b91b305bf736d7aff02e20a1
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-12-18 14:39:19 +0900

    add footer and link

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         contentslist: ["about", "skills", "event", "interests", "links"],
-        linklist: ["https://twitter.com/ei01241", "http://ei0124.blog.fc2.com/", "https://github.com/ei0410"]
+        linklist: [
+            { url: "https://twitter.com/ei01241", text: "twitter" },
+            { url: "https://github.com/ei0410", text: "github" },
+            { url: "http://ei0124.blog.fc2.com", text: "blog" },
+        ]
     }
 })
